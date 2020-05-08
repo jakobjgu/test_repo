e0f10416a14dc124d2a656dad16625323b2fef9f
Jakob <61227884+jakobjgu@users.noreply.github.com>
2020-05-08 06:57:17 -0500

    played around with linter package for atom

---
+++ lint_testing.py
@@ -0,0 +1,11 @@
+def test_the_linter(a=int, b=int):
+    "argkljnasdfklbnaskd b"
+    c = a+b
+    return c
+
+
+def ABCDEFG():
+    new_string = 'are doc-strings not enforced?'
+    new_string2 = 'can function names be capitalized?'
+    print(new_string)
+    print('\n'+new_string)
