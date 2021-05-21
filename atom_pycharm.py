0b3e742e449b0f40ea051da4e3f9d3dc0cd2b57e
Jakob <61227884+jakobjgu@users.noreply.github.com>
2021-05-21 16:13:47 +0300

    testing atom and pycharm

---
+++ atom_pycharm.py
@@ -0,0 +1,14 @@
+"""
+this is s short test to see if Atom and PyCharm can work together on Git.
+Or rather, if I am able to make them work together, because I bet the problem
+is gonna be me and not them.
+"""
+
+
+def add_stuff(a=int, b=int):
+    """
+    testing stuff
+    """
+    c = a + b
+    return c
+
